fff61a73993bfedb90b55d8bcce9e27854b0f4aa
monicagt <monicagt@gmail.com>
2022-12-05 09:59:28 -0500

    added

--- src/main/java/com/example/testdeploya/TestDeployAApplication.java
+++ src/main/java/com/example/testdeploya/TestDeployAApplication.java
@@ -2,12 +2,26 @@ package com.example.testdeploya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @SpringBootApplication
 public class TestDeployAApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TestDeployAApplication.class, args);
+	}
+
+	@GetMapping("/welcome")
+	public String appWelcome() {
+		return "welcome to my APY." ;
+	}
+
+	@GetMapping("/names")
+	public List<String> showNames() {
+		return List.of("Sam", "Monica", "Maria");
 	}
 
 }
