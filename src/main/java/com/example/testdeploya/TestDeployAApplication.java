024b2ec71e45deae5b00c4bfd8417cbdb297c36b
monicagt <monicagt@gmail.com>
2022-12-05 10:59:43 -0500

    First commit

--- src/main/java/com/example/testdeploya/TestDeployAApplication.java
+++ src/main/java/com/example/testdeploya/TestDeployAApplication.java
@@ -3,10 +3,12 @@ package com.example.testdeploya;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class TestDeployAApplication {
 
 	public static void main(String[] args) {
