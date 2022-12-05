710a4b88acff752cc9fe15577cc7fd9ac635b1d0
monicagt <monicagt@gmail.com>
2022-12-05 09:54:16 -0500

    first commit

---
+++ src/main/java/com/example/testdeploya/TestDeployAApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdeploya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeployAApplication.class, args);
+	}
+
+}
